98ae449e146b1dbbe31fab07380e9f3e79011c0c
sato-souma <59986133+sato-souma@users.noreply.github.com>
2022-02-07 12:09:12 +0900

    eslint prettier 設定

--- src/api/taskList.ts
+++ src/api/taskList.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express'
 
-const router = express.Router();
+const router = express.Router()
 
-router.get("/getTask", (req: express.Request, res: express.Response) => {
-  res.send("taskliskcjanc");
-});
+router.get('/getTask', (req: express.Request, res: express.Response) => {
+  res.send('taskliskcjanc')
+})
 
-export default router;
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,21 @@
-"use strict";
+'use strict'
 
-import express from "express";
-import getTaskRouter from "./api/taskList";
+import express from 'express'
+import getTaskRouter from './api/taskList'
 
 // Constants
-const PORT: number = 8080;
-const HOST: string = "0.0.0.0";
+const PORT: number = 8080
+const HOST: string = '0.0.0.0'
 
 // App
-const app = express();
+const app = express()
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("Hello World ~");
-});
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World ~')
+})
 
 // Task取得
-app.use(getTaskRouter);
+app.use(getTaskRouter)
 
 // Task登録
 
@@ -23,5 +23,5 @@ app.use(getTaskRouter);
 
 // Task修正
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
